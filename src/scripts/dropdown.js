cc893ac2d5aa5cef876c6869a481133babd1291c
Hilda <hilda.kh.amiri@gmail.com>
2020-05-16 03:48:49 -0700

    all required functionality finished. Might need slight refactoring

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -2,24 +2,54 @@ const dropdownPrompt = document.querySelector(".drop-down-container__prompt");
 
 const dropdownOptionsBox = document.querySelector(".drop-down-container__options");
 
-const dropDownImage = document.querySelector(".drop-down-container__image-box");
+const imageBox = document.querySelector(".drop-down-container__image-box");
+
+const dropDownImage = document.querySelector(".image-box__image");
 
 const toggleDropdownOptions = (e) => {
   // e.preventDefault();
   dropdownOptionsBox.classList.toggle('hidden');
-  dropDownImage.classList.toggle('hidden');
+  imageBox.classList.toggle('hidden');
 };
 
 dropdownPrompt.addEventListener('click', toggleDropdownOptions);
+
 
 const dropdownOption = document.querySelectorAll(".options__option");
 const promptText = document.querySelector(".prompt__text");
 
 const selectOptionFromDropdown = (e) => {
-  if((e.target.innerText === "Dog with shades") || (e.target.innerText === "Storm Trooper") || (e.target.innerText === "Pinneapple Vacation") || (e.target.innerText === "Donuts" ))
-  {
-    promptText.innerText = e.target.innerText;
-    toggleDropdownOptions();
+  console.log(dropDownImage)
+  if(e.target.innerText === "Dog with shades") {
+    promptText.innerText = "Dog with shades";
+    dropDownImage.src = "./assets/dog-with-shades.png";
+    setTimeout( () => {
+      toggleDropdownOptions();
+    }, 200)
+  }
+
+  if(e.target.innerText === "Storm Trooper") {
+    promptText.innerText = "Storm Trooper";
+    dropDownImage.src = "./assets/storm-trooper.png";
+    setTimeout( () => {
+      toggleDropdownOptions();
+    }, 200)
+  }
+
+  if(e.target.innerText === "Pineapple Vacation") {
+    promptText.innerText = "Pineapple Vacation";
+    dropDownImage.src = "./assets/pineapple-vacation.png";
+    setTimeout( () => {
+      toggleDropdownOptions();
+    }, 200)
+  }
+
+  if(e.target.innerText === "Donuts") {
+    promptText.innerText = "Donuts";
+    dropDownImage.src = "./assets/donuts.png";
+    setTimeout( () => {
+      toggleDropdownOptions();
+    }, 200)
   }
 }
 
@@ -27,7 +57,6 @@ dropdownOption.forEach(() => {
     addEventListener('click', selectOptionFromDropdown);
 })
 
-const dropdownImage = document.querySelector(".image-box__image");
 
 
 
