b1fae5a8a2c171a44b1e26dc61134e5abd3ea107
Hilda <hilda.kh.amiri@gmail.com>
2020-05-15 23:09:24 -0700

    drop down toggle function modified to hide image when drop down is visible

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -3,13 +3,17 @@ const dropdownPrompt = document.querySelector(".drop-down-container__prompt");
 
 const dropdownOptionsBox = document.querySelector(".drop-down-container__options");
 
+const dropDownImage = document.querySelector(".drop-down-container__image-box");
+
 const toggleDropdownOptions = () => {
   console.log("clicked", dropdownOptionsBox.style)
 
     if(dropdownOptionsBox.style.display == "block") {
     dropdownOptionsBox.style.display = "none";
+    dropDownImage.style.display ="block";
   } else {
     dropdownOptionsBox.style.display = "block";
+    dropDownImage.style.display = "none"
   }
 };
 
