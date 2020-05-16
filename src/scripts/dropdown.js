e311cd767b8894582b4a8d6e5f7d1a8e29e83798
Hilda <hilda.kh.amiri@gmail.com>
2020-05-15 20:38:28 -0700

    initial function to change box text for drop down items click writte. query selector bug needs fixing

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,9 +1,27 @@
-const toggleDropdownOptions = (e) => {
-  const dropdownOptions = document.querySelector(".drop-down-container__options");
 
-  if(dropdownOptions.style.display == "none") {
-    dropdownOptions.style.display = "block";
+const dropdownPrompts = document.querySelector(".drop-down-container__prompt");
+
+const dropdownOptionsBox = document.querySelector(".drop-down-container__options");
+
+const toggleDropdownOptions = (e) => {
+    if(dropdownOptionsBox.style.display == "block") {
+    dropdownOptionsBox.style.display = "none";
   } else {
-    dropdownOptions.style.display = "none";
+    dropdownOptionsBox.style.display = "block";
   }
 };
+
+dropdownPrompts.addEventListener('click', toggleDropdownOptions);
+
+
+const dropdownOption = document.querySelectorAll(".options__option");
+const promptText = document.querySelector(".prompt__text");
+
+const selectBusinessModelImage = (e) => {
+  // console.log(e.target.innerHTML, promptText.innerHTML)
+  promptText.innerHTML = e.target.innerHTML;
+}
+
+dropdownOption.forEach(option => {
+  addEventListener('click', selectBusinessModelImage);
+})
