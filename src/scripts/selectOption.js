07f6eeac7cf43e7adc4e4e65a95ae4ef533a2cf1
Hilda <hilda.kh.amiri@gmail.com>
2020-05-16 17:17:57 -0700

    Submission2. emplty alt tag on image modified. Altering image alt on option select added to js selectOption function.

--- src/scripts/selectOption.js
+++ src/scripts/selectOption.js
@@ -22,6 +22,7 @@ const selectOption = (e) => {
   if (e.target.innerText === "Pizza and bears") {
     promptText.innerText = "Pizza and bears";
     dropDownImage.src = "./assets/pizza-and-bears.png";
+    dropDownImage.alt = "Pizza and bears";
     setTimeout(() => {
       hideOptionsShowImage();
     }, 20);
@@ -30,6 +31,7 @@ const selectOption = (e) => {
   if (e.target.innerText === "Dog with shades") {
     promptText.innerText = "Dog with shades";
     dropDownImage.src = "./assets/dog-with-shades.png";
+    dropDownImage.alt = "Dog with shades";
     setTimeout(() => {
       hideOptionsShowImage();
     }, 20);
@@ -38,6 +40,7 @@ const selectOption = (e) => {
   if (e.target.innerText === "Storm Trooper") {
     promptText.innerText = "Storm Trooper";
     dropDownImage.src = "./assets/storm-trooper.png";
+    dropDownImage.alt = "Storm Trooper";
     setTimeout(() => {
       hideOptionsShowImage();
     }, 20);
@@ -46,6 +49,7 @@ const selectOption = (e) => {
   if (e.target.innerText === "Pineapple Vacation") {
     promptText.innerText = "Pineapple Vacation";
     dropDownImage.src = "./assets/pineapple-vacation.png";
+    dropDownImage.alt = "Pineapple Vacation";
     setTimeout(() => {
       hideOptionsShowImage();
     }, 20);
@@ -54,6 +58,7 @@ const selectOption = (e) => {
   if (e.target.innerText === "Donuts") {
     promptText.innerText = "Donuts";
     dropDownImage.src = "./assets/donuts.png";
+    dropDownImage.alt = "Donuts"
     setTimeout(() => {
       toggleOptions();
     }, 20);
